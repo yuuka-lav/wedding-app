f2606dbdf42cca378fea3c089ee1b251297dae63
yuuka-lav <60505747+yuuka-lav@users.noreply.github.com>
2020-02-19 12:47:15 +0900

    change top_page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root "halls#index"
-  resources :toppages, only: [:index]
+  resources :top_page, only: [:index]
   resources :users, only: [:edit, :update]
   resources :halls, only: [:index] 
 end
