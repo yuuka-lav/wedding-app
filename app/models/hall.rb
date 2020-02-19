34cc74478ef8e1ece5ed3c5e5f0cc31685a79bc0
yuuka-lav <60505747+yuuka-lav@users.noreply.github.com>
2020-02-19 15:28:50 +0900

    add config

--- app/models/hall.rb
+++ app/models/hall.rb
@@ -2,7 +2,7 @@ class Hall < ApplicationRecord
   belongs_to :user
   has_many :likes, dependent: :destroy
 
-  # def like_user(user_id)
-  #   likes.find_by(user_id: user_id)
-  # end
+  def like_user(user_id)
+    likes.find_by(user_id: user_id)
+  end
 end
