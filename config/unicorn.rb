f3aa16436390001f78dd071e00c30ba9a4163ce9
yuuka-lav <60505747+yuuka-lav@users.noreply.github.com>
2020-02-27 23:20:25 +0900

    Update unicorn.rb

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -7,11 +7,10 @@ worker_processes 1
 #アプリケーションの設置されているディレクトリを指定
 working_directory "#{app_path}/current"
 
-pid "#{app_path}/shared/tmp/pids/unicorn.pid"
 listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
+pid "#{app_path}/shared/tmp/pids/unicorn.pid"
 stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
 stdout_path "#{app_path}/shared/log/unicorn.stdout.log"
-
 
 #Railsアプリケーションの応答を待つ上限時間を設定
 timeout 60
