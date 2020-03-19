3ab2e56ab09ce27f02001273a5d086d318782190
yuuka-lav <60505747+yuuka-lav@users.noreply.github.com>
2020-03-19 16:14:20 +0900

    rannking/edit

--- app/controllers/halls_controller.rb
+++ app/controllers/halls_controller.rb
@@ -6,5 +6,6 @@ class HallsController < ApplicationController
     else
       @halls = Hall.all
     end
+    @all_ranks = Hall.find(Like.group(:hall_id).order('count(hall_id) desc').limit(3).pluck(:hall_id))
   end
 end
