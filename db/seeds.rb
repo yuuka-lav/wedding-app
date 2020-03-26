b65f2b6a842ffe9fe30d5498dac34d4aee99b8f8
yuuka-lav <60505747+yuuka-lav@users.noreply.github.com>
2020-03-26 17:20:51 +0900

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,17 +17,17 @@ Hall.create!(
     {
       name: '< Wedding大阪 >',
       place: '｜ 専門式場 / 大阪府 ｜ 収容人数：20 ~ 120人 |',
-      image: File.open('./app/assets/images/wedding-6.jpg'),
+      image: './app/assets/images/wedding-6.jpg',
     },
     {
       name: '< Wedding難波 >',
       place: '｜ ホテル / 大阪府 ｜ 収容人数：80 ~ 200人 |',
-      image: File.open('./app/assets/images/wedding-6.jpg'),
+      image: './app/assets/images/wedding-6.jpg',
     },
     {
       name: '< Wedding埼玉 >',
       place: '｜ ホテル / 埼玉県 ｜ 収容人数：100 ~ 180人 |',
-      image: File.open('./app/assets/images/wedding-9.jpg'),
+      image: './app/assets/images/wedding-9.jpg',
     }
   ]
 )
